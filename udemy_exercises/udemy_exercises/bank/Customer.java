1f93caeadfb9fe08f318b94f2a096cb6fd667285
Zachary Johnson <zach_j@att.net>
2020-06-22 20:39:21 -0500

    finished 3rd exercise

--- udemy_exercises/udemy_exercises/bank/Customer.java
+++ udemy_exercises/udemy_exercises/bank/Customer.java
@@ -1,7 +1,29 @@
 package udemy_exercises.bank;
 
-public class Customer {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class Customer {
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initialAmount){
+        this.name = name;
+        this.transactions = new ArrayList<Double>();
+        addTransaction(initialAmount);
+    }
+
+    public void addTransaction(double amount){
+        this.transactions.add(amount);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions(){
+        return transactions;
+    }
 }
 
 /*
